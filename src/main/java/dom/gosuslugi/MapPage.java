d047c12e05095c2d2cd20c5fb406872159399a1e
Tzeeka <Tzeeka@gmail.com>
2018-05-16 19:35:29 +0500

    version 0.2

--- src/main/java/dom/gosuslugi/MapPage.java
+++ src/main/java/dom/gosuslugi/MapPage.java
@@ -4,24 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class MapPage {
 
     WebDriver driver;
+    Actions action;
 
     public MapPage(WebDriver driver) {
+
         this.driver = driver;
+        this.action = new Actions(driver);
     }
 
     String pageHeader = "h1.ng-binding";
     String findHome = "body > div.page-wrapper.page-wrapper_v2.ng-scope > div:nth-child(7) > div > div > div.app-map > div.app-map-header > div > div > div.col-md-2 > span:nth-child(1)";
     String findButtonSelector = "button.btn.btn-default";
-
-    String inputFormSelector = "input.form-control.ng-valid.clearable.ng-dirty";
     String show = "span.show-home";
 
     String searchFieldSelector = "body > div.page-wrapper.page-wrapper_v2.ng-scope > div:nth-child(7) > div > div > div.app-map > div.app-map-header > div > div > div.col-md-10 > form.form-inline.find-by-address.form-base.ng-pristine.ng-valid > div.form-group > div > input";
@@ -36,10 +36,18 @@ public class MapPage {
     String roSelector = "/html/body/div[2]/div[6]/div/div/div[2]/div[1]/div/div/div[2]/form[1]/div[5]/label/span";
     String omsSelector = "/html/body/div[2]/div[6]/div/div/div[2]/div[1]/div/div/div[2]/form[1]/div[6]/label/span";
     String reestrHouseSelector = "/html/body/div[2]/div[6]/div/div/div[2]/div[1]/div/div/div[2]/form[1]/div[7]/label/span";
-
     String searchResultElem = "ul.list-unstyled.list-type-0";
 
+    String mapArea = "//*[@id=\"map\"]/ymaps/ymaps/ymaps/ymaps[1]";
+    String scaleBar = "//*[@id=\"map\"]/ymaps/ymaps/ymaps/ymaps[4]/ymaps[3]/ymaps/ymaps";
+    String goBack = "/html/body/div[2]/div[6]/div/div/div[2]/div[2]/div/div[2]/div/a/span";
 
+
+    String houseCard = "#map > ymaps > ymaps > ymaps.ymaps-2-1-64-inner-panes > ymaps.ymaps-2-1-64-balloon-pane > ymaps > ymaps > ymaps > ymaps.ymaps-2-1-64-balloon__layout > ymaps.ymaps-2-1-64-balloon__content";
+    String houseButtonOne = "ymaps > a:nth-child(5)";
+    String houseButtonTwo = "ymaps > a:nth-child(7)";
+    String houseTypeLink = "ymaps > li.search-company-name > a";
+    String houseAdress = "ymaps > li.search-company-address";
 
 
     // WebElement searchField = driver.findElement(By.cssSelector(searchFieldSelector));
@@ -73,14 +81,7 @@ public class MapPage {
     }
 
 
-    public boolean isCheckboxOnetPresent() {
-        try {driver.findElement(By.xpath(checkBox1));
-        }
-        catch (NoSuchElementException e){
-            return false;
-        }
-        return true;
-    }
+
 
     public boolean checkBox1Selected() {
 
@@ -112,8 +113,9 @@ public class MapPage {
         List<WebElement> elements = driver.findElements(By.cssSelector(searchResultElem));
         try {
             for (WebElement el : elements) {
-                el.findElement(By.xpath("a.ng-binding"));
-                el.findElement(By.cssSelector("li.search-company-address.ng-binding.ng-scope"));
+                el.findElement(By.cssSelector("a.ng-binding")).getAttribute("class");
+                el.findElement(By.cssSelector("li.search-company-address.ng-binding.ng-scope")).getAttribute("class");
+
             }
         } catch (NoSuchElementException e) {
             return false;
@@ -121,13 +123,50 @@ public class MapPage {
         return true;
     }
 
+    public boolean yandexMapIsPresent() {
+        try {
+            driver.findElement(By.xpath(mapArea));
+            driver.findElement(By.xpath(scaleBar));
 
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
 
+    public boolean goBackIsPresent() {
+        try{
+            driver.findElement(By.xpath(goBack));
+        } catch (NoSuchElementException e){
+            return false;
+        }
+        return true;
+    }
 
+    public void moveToHouseClick() {
 
+            WebElement yamap = driver.findElement(By.xpath(mapArea));
+            action.moveToElement(yamap, 430, 280).click().build().perform();
+    }
 
+    public boolean checkHouseCardElements() {
+        try {
+            driver.findElement(By.cssSelector(houseCard));
+            driver.findElement(By.cssSelector(houseButtonOne));
+           // driver.findElement(By.cssSelector(houseButtonTwo));
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
 
+    public String houseType() {
+          return driver.findElement(By.cssSelector(houseTypeLink)).getText();
+    }
 
+    public String houseAdress() {
+        return driver.findElement(By.cssSelector(houseAdress)).getText();
+    }
 
 
     public MapPage clickFourBoxes() {
@@ -138,14 +177,30 @@ public class MapPage {
         return this;
     }
 
-    public MapPage findChe(String text) {
-        driver.findElement(By.cssSelector(searchFieldSelector)).sendKeys(text);
+    public MapPage clickBox1() {
+        driver.findElement(By.xpath(checkBox1)).click();
         return this;
     }
 
+    public MapPage clickBox2() {
+        driver.findElement(By.xpath(checkBox2)).click();
+        return this;
+    }
 
+    public MapPage clickBox3() {
+        driver.findElement(By.xpath(checkBox3)).click();
+        return this;
+    }
 
+    public MapPage clickBox4() {
+        driver.findElement(By.xpath(checkBox2)).click();
+        return this;
+    }
 
+    public MapPage clickBox5() {
+        driver.findElement(By.xpath(checkBox2)).click();
+        return this;
+    }
 
 
 }
--- src/test/java/dom/gosuslugi/MainTest.java
+++ src/test/java/dom/gosuslugi/MainTest.java
@@ -9,7 +9,6 @@ public class MainTest extends BaseTest {
 
     private String header = "Поиск объектов на карте";
     private String findHome = "Найти дом по адресу";
-    private String searchWord = "ч";
 
     private String houseName = "Дома";
     private String oumkdName = "Организации,\n" + "управляющие МКД";
@@ -17,28 +16,34 @@ public class MainTest extends BaseTest {
     private String omsName = "Органы местного\n" + "самоуправления";
     private String reestrHouseName = "Дома в реестре лицензий,\n" + "размещенные по ст. 198 ЖК РФ";
 
+    private String houseTypeLink = "Многоквартирный дом";
+    private String houseAdress = "105062, Москва г, ул. Покровка, д. 31, строение 1";
+
 
     @Test
     public void test() throws Exception {
         MainPage mainpage = new MainPage(driver);
         MapPage map = mainpage.goToMap();
 
-       Assert.assertEquals(map.getHeader(),header);
-       Assert.assertEquals(map.getFindHome(), findHome);
+        Assert.assertEquals(map.getHeader(),header);
+        Assert.assertEquals(map.getFindHome(), findHome);
 
-       Assert.assertTrue(map.isCheckboxOnetPresent());
-       Assert.assertTrue(map.findBlockIsPresent());
-       Assert.assertTrue(map.checkBox1Selected());
+        Assert.assertTrue(map.findBlockIsPresent());
+        Assert.assertTrue(map.checkBox1Selected());
 
-       Assert.assertEquals(map.houseGetName(), houseName);
-       Assert.assertEquals(map.oumkdGetName(), oumkdName);
-       Assert.assertEquals(map.roGetName(), roName);
-       Assert.assertEquals(map.omsGetName(), omsName);
-       Assert.assertEquals(map.reestrHouseGetName(), reestrHouseName);
+        Assert.assertEquals(map.houseGetName(), houseName);
+        Assert.assertEquals(map.oumkdGetName(), oumkdName);
+        Assert.assertEquals(map.roGetName(), roName);
+        Assert.assertEquals(map.omsGetName(), omsName);
+        Assert.assertEquals(map.reestrHouseGetName(), reestrHouseName);
 
-       Assert.assertTrue(map.searchResultsPresent());
+        Assert.assertTrue(map.searchResultsPresent());
+        Assert.assertTrue(map.yandexMapIsPresent());
+        Assert.assertTrue(map.goBackIsPresent());
 
-
-
+        map.moveToHouseClick();
+        Assert.assertTrue(map.checkHouseCardElements());
+        Assert.assertEquals(map.houseType(), houseTypeLink);
+        Assert.assertEquals(map.houseAdress(), houseAdress);
     }
 }
